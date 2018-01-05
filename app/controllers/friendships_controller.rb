21d506c174016333d3dbc36e145cdc6a1733a9da
finile <wu.nile@gmail.com>
2018-01-05 22:22:36 +0800

    add unfriend button on user index page and destroy action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -7,7 +7,16 @@ class FriendshipsController < ApplicationController
        redirect_back fallback_location: root_path, notice: "成功加入好友"
       else
         flash[:alert] = @friendship.errors.full_messages.to_sentence
-        redirect_back fallback_location: root_path
+        redirect_back(fallback_location: root_path) 
       end 
   end
+
+  def destroy
+    @friendship = current_user.friendships.where(friend_id: params[:id]).first
+    @friendship.destroy
+    flash[:alert] = "成功刪除好友"
+    redirect_back(fallback_location: root_path)
+
+  end
+
 end
