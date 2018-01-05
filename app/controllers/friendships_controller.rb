6e418bfd57768a86ae95f6bca384f4698011399a
finile <wu.nile@gmail.com>
2018-01-05 22:08:55 +0800

    add user index page with friend button, validate uniqueness of following_id in user_id scope and add create action for followship record

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,13 @@
 class FriendshipsController < ApplicationController
+
+
+  def create
+    @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+      if @friendship.save
+       redirect_back fallback_location: root_path, notice: "成功加入好友"
+      else
+        flash[:alert] = @friendship.errors.full_messages.to_sentence
+        redirect_back fallback_location: root_path
+      end 
+  end
 end
